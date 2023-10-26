277a6cd9e95bf85978e49217ed8b5d0a1fc501d3
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-10-26 22:19:37 +0200

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNum = 0;
+        this.maxNum = 0;
+    }
 
     setRange(min, max) {
-
+        this.minNum = min;
+        this.maxNum = max;
     }
 
     guess() {
-
+        const middle = Math.round(this.minNum / 2 + this.maxNum / 2);
+        return middle;
     }
 
     lower() {
-
+        this.maxNum = Math.round(this.minNum / 2 + this.maxNum / 2);
     }
 
     greater() {
-
+        this.minNum = Math.round(this.minNum / 2 + this.maxNum / 2);
     }
 }
 
